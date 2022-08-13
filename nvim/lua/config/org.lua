de724db7c418d16cc6e4cbda44c470ab94a62bbe
Szymon Solak <16944591+szymon-solak@users.noreply.github.com>
2022-08-13 11:55:58 +0200

    feat: Add basic org capture templates

--- nvim/lua/config/org.lua
+++ nvim/lua/config/org.lua
@@ -4,6 +4,20 @@ if not status_ok then
 end
 
 orgmode.setup {
-	org_default_notes_file = '~/projects/notebook/scratch.org'
+	org_default_notes_file = '~/projects/notebook/scratch.org',
+	org_capture_templates = {
+		t = "Todo",
+		tp = {
+			description = 'Personal Todo',
+			template = '* TODO %?\n %u',
+			target = '~/projects/notebook/todo.org',
+		},
+		tw = {
+			description = 'Work Todo',
+			template = '* TODO %?\n %u',
+			target = '~/projects/notebook/work-todo.org',
+		}
+	}
 }
+
 orgmode.setup_ts_grammar()
